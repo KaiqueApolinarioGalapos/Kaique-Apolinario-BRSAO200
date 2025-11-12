523f3e4511e597e7cf4dd8a2c5bfb717926483de
Kaique Apolinario Da Silva <kas@galapos.com.br>
2025-11-12 09:54:06 -0300

    Resultado da atividade prática 01

---
+++ questao-04.py
@@ -0,0 +1,14 @@
+"""4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final."""
+
+nomeDoProduto = "Cadeira Infantil"
+preco = 12.4
+quantia = 3
+total = preco * quantia
+
+print(f"Nome do produto: {nomeDoProduto}. \nPreço unitário: R$ {preco}. \nQuantidade: {quantia}. \nPortanto, o valor total dessa compra será de: {total}")
